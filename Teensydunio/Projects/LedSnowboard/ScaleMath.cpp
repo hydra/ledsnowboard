730e5f93b6ff9d7b72a8e94ec28139b174c252f7
Dominic Clifton <me@dominicclifton.name>
2014-01-30 20:22:05 +0000

    Updating scaleRange so it can work with negative-to-positive ranges such as -32 to +32 in addition to positive-to-positive ranges such as 0 - 64.

--- Teensydunio/Projects/LedSnowboard/ScaleMath.cpp
+++ Teensydunio/Projects/LedSnowboard/ScaleMath.cpp
@@ -3,5 +3,5 @@ int scaleRange(int x, int srcMin, int srcMax, int destMin, int destMax) {
 
   long int a = ((long int) destMax - (long int) destMin) * ((long int) x - (long int) srcMin);
   long int b = (long int) srcMax - (long int) srcMin;
-  return (a / b) + destMin;
+  return ((a / b) - (destMax - destMin)) + destMax;
 }
